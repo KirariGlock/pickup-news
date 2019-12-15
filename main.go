4c6636bf9f65e074780f1168c2df31a0a95e2e6d
Kitazawa <javafxscript20072011@gmail.com>
2019-12-15 19:00:46 +0900

    Add print api result count

--- main.go
+++ main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/url"
 	"os"
 	"time"
 )
+
+type Env struct {
+	Keyword string
+	From    string
+	To      string
+}
 
 func main() {
 	resuest, err := http.NewRequest("GET", "https://newsapi.org/v2/everything", nil)
@@ -15,8 +23,10 @@ func main() {
 		os.Exit(1)
 	}
 
+	keyword := "Google"
+
 	values := url.Values{}
-	values.Add("qInTitle", "Google")
+	values.Add("qInTitle", keyword)
 	values.Add("from", "2019-12-07")
 	values.Add("to", "2019-12-07")
 	values.Add("apiKey", "") // TODO 環境変数で渡す
@@ -28,10 +38,24 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
+	} else if resp.StatusCode != 200 {
+		fmt.Printf("Unable to get this url : http status is %d \n", resp.StatusCode)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
 
-	// TODO
+	naResp := new(NewsAPIRespons)
+	if err := json.Unmarshal(body, &naResp); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
+	fmt.Printf("Keyword: %s resultCount: %d", keyword, naResp.TotalResults)
 }
 
 type NewsAPIRespons struct {
