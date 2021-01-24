ea766718e2de52956a45dfc85f385568b6bd40a8
Kitazawa <javafxscript20072011@gmail.com>
2021-01-24 16:09:31 +0900

    Added pickup keys form S3 bucket

--- main.go
+++ main.go
@@ -25,12 +25,17 @@ type Env struct {
 }
 
 type RequestParameter struct {
-	Keyword          string
-	From             string
-	To               string
-	NoticeLowerLimit int `default:"0"` // Don't notify if the number of news is below NoticeLowerLimit
-	S3BacketName     string
-	S3ObjectKey      string
+	From         string
+	To           string
+	S3BacketName string
+	S3ObjectKey  string
+}
+
+type PickupKey struct {
+	Keyword string `json:"keyword"`
+
+	// Don't notify if the number of news is below NoticeLowerLimit
+	NoticeLowerLimit int `json:"noticeLowerLimit"`
 }
 
 func main() {
@@ -45,7 +50,8 @@ func HandleRequest(ctx context.Context, rp RequestParameter) (string, error) {
 		os.Exit(1)
 	}
 
-	rp = initRequestParameter(rp)
+	p := initRequestParameter(&rp)
+	keys := loadPickupKeys(&rp)
 
 	// create request
 	resuest, err := http.NewRequest("GET", "http://newsapi.org/v2/everything", nil)
@@ -54,57 +60,60 @@ func HandleRequest(ctx context.Context, rp RequestParameter) (string, error) {
 		os.Exit(1)
 	}
 
-	values := url.Values{}
-	values.Add("qInTitle", rp.Keyword)
-	values.Add("from", rp.From)
-	values.Add("to", rp.To)
-	values.Add("apiKey", env.Apikey)
-	resuest.URL.RawQuery = values.Encode()
+	for _, key := range *keys {
+		values := url.Values{}
+		values.Add("qInTitle", key.Keyword)
+		values.Add("from", p.From)
+		values.Add("to", p.To)
+		values.Add("apiKey", env.Apikey)
+		resuest.URL.RawQuery = values.Encode()
 
-	// execute NewsAPI
-	client := new(http.Client)
-	resp, err := client.Do(resuest)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	} else if resp.StatusCode != 200 {
-		fmt.Printf("Unable to get this url : http status is %d \n", resp.StatusCode)
+		// execute NewsAPI
+		client := new(http.Client)
+		resp, err := client.Do(resuest)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		} else if resp.StatusCode != 200 {
+			fmt.Printf("Unable to get this url : http status is %d \n", resp.StatusCode)
+		}
+		defer resp.Body.Close()
+
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		naResp := new(NewsAPIRespons)
+		if err := json.Unmarshal(body, &naResp); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		if naResp.TotalResults <= key.NoticeLowerLimit {
+			return fmt.Sprintf("TotalResult is lower NoticeLowerLimit. TotalResult:%d, NoticeLowerLimit:%d\n", naResp.TotalResults, key.NoticeLowerLimit), nil
+		}
+
+		messageHeader := "<!channel> Keyword: " + key.Keyword + " resultCount: " + strconv.Itoa(naResp.TotalResults) + " from: " + p.From + " to: " + p.To + "\n"
+		var messageDetail bytes.Buffer
+		for i, article := range naResp.Articles {
+			messageDetail.WriteString("No.")
+			messageDetail.WriteString(strconv.Itoa(i + 1))
+			messageDetail.WriteString(", ")
+			messageDetail.WriteString(article.Title)
+			messageDetail.WriteString(", ")
+			messageDetail.WriteString(article.URL)
+			messageDetail.WriteString("\n")
+		}
+
+		notificationSlack(env, messageHeader+messageDetail.String())
 	}
-	defer resp.Body.Close()
 
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	naResp := new(NewsAPIRespons)
-	if err := json.Unmarshal(body, &naResp); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	if naResp.TotalResults <= rp.NoticeLowerLimit {
-		return fmt.Sprintf("TotalResult is lower NoticeLowerLimit. TotalResult:%d, NoticeLowerLimit:%d\n", naResp.TotalResults, rp.NoticeLowerLimit), nil
-	}
-
-	messageHeader := "<!channel> Keyword: " + rp.Keyword + " resultCount: " + strconv.Itoa(naResp.TotalResults) + " from: " + rp.From + " to: " + rp.To + "\n"
-	var messageDetail bytes.Buffer
-	for i, article := range naResp.Articles {
-		messageDetail.WriteString("No.")
-		messageDetail.WriteString(strconv.Itoa(i + 1))
-		messageDetail.WriteString(", ")
-		messageDetail.WriteString(article.Title)
-		messageDetail.WriteString(", ")
-		messageDetail.WriteString(article.URL)
-		messageDetail.WriteString("\n")
-	}
-
-	notificationSlack(env, messageHeader+messageDetail.String())
 	return "Success notification.", nil
 }
 
-func initRequestParameter(rp RequestParameter) RequestParameter {
+func initRequestParameter(rp *RequestParameter) *RequestParameter {
 	t := time.Now().UTC()
 	loc, err := time.LoadLocation("Asia/Tokyo")
 	if err != nil {
@@ -143,7 +152,17 @@ func notificationSlack(env Env, message string) {
 	defer resp.Body.Close()
 }
 
-func readS3File(rp RequestParameter) string {
+func loadPickupKeys(rp *RequestParameter) *[]PickupKey {
+	jsonBytes := readS3File(rp)
+	var pickupKeys []PickupKey
+	if err := json.Unmarshal(jsonBytes, &pickupKeys); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	return &pickupKeys
+}
+
+func readS3File(rp *RequestParameter) []byte {
 	sess := session.Must(session.NewSessionWithOptions(session.Options{
 		Profile:           "di",
 		SharedConfigState: session.SharedConfigEnable,
@@ -162,7 +181,7 @@ func readS3File(rp RequestParameter) string {
 	defer obj.Body.Close()
 	brb := new(bytes.Buffer)
 	brb.ReadFrom(obj.Body)
-	return brb.String()
+	return brb.Bytes()
 }
 
 type NewsAPIRespons struct {
